a75983182b82dbf2f44d7b70aa544a46159dc476
me <some_email>
2020-02-13 13:13:03 +0300

    big initial commit

---
+++ src/main/java/letscode/sarafan/controller/MessageController.java
@@ -0,0 +1,51 @@
+package letscode.sarafan.controller;
+
+import letscode.sarafan.domain.Message;
+import letscode.sarafan.repo.MessageRepo;
+import org.springframework.beans.BeanUtils;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+// import org.springframework.beans.factory.annotation.Autowired;
+
+@RestController
+@RequestMapping("message")
+public class MessageController {
+    private final MessageRepo messageRepo;
+
+    // @Autowired
+    public MessageController(MessageRepo messageRepo) {
+        this.messageRepo = messageRepo;
+    }
+
+    @GetMapping
+    public List<Message> list() {
+        return messageRepo.findAll();
+    }
+
+    @GetMapping("{id}")
+    public Message getOne(@PathVariable("id") Message message) {
+        return message;
+    }
+
+    // fetch('/message', { method: 'POST', headers: { 'Content-Type': 'application/json' }, body: JSON.stringify( { text: 'Fourth message' } ) }).then(console.log)
+    @PostMapping
+    public Message create(@RequestBody Message message) {
+        return messageRepo.save(message);
+    }
+
+    // fetch('/message/4', { method: 'PUT', headers: { 'Content-Type': 'application/json' }, body: JSON.stringify( { text: 'Fourth message (4)', id: '10' } ) }).then(console.log)
+    @PutMapping("{id}")
+    public Message update(@PathVariable("id") Message messageFromDb,
+                                      @RequestBody Message message) {
+        BeanUtils.copyProperties(message, messageFromDb, "id");
+        return messageRepo.save(messageFromDb);
+    }
+
+    // fetch('/message/4', { method: 'DELETE' }).then(console.log)
+    @DeleteMapping("{id}")
+    public void delete(@PathVariable("id") Message message) {
+        messageRepo.delete(message);
+    }
+}
